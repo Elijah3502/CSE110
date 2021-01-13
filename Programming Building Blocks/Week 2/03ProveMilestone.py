7e52aa8b6f709fe22f2e9be4661e124bb64f9675
Elijah Price <46971329+Elijah3502@users.noreply.github.com>
2021-01-12 22:12:36 -0700

    Completed 03Teach

---
+++ Programming Building Blocks/Week 2/03ProveMilestone.py
@@ -0,0 +1,63 @@
+"""
+Need to find : 
+
+Price of a childs meal : Float : 
+Price of an adult meal : Float : 
+number of children : int : 
+number of adults : int : 
+sales tax : float :
+"""
+child_sub = 0.0
+adult_sub = 0.0
+sub_total = 0.0
+total = 0.0
+sales_tax = 0.0
+tip_total = 0.0
+total = 0.0
+change = 0.0
+#GET CHILD MEAL COST
+child_cost = float(input("What is the cost of a child's meal? : $ "))
+#GET ADULT MEAL COST
+adult_cost = float(input("What is the cost of an Adult's meal? : $ "))
+#GET NUMBER OF CHILDREN
+child_num = int(input("How many children are there? : "))
+#GET NUMBER OF ADULTS
+adult_num = int(input("How many adults are there? : "))
+#GET SALES TAX
+tax_rate = float(input("What is the sales tax rate? : %"))
+#COMPUTE SUBTOTAL(BEFORE TAXES)
+child_sub = child_cost * child_num
+adult_sub = adult_cost * adult_num
+sub_total = child_sub + adult_sub
+#DISPLAY SUB TOTAL
+print("\n\nSubtotal : $ " + str(sub_total))
+#COMPUTE SALES TAX
+sales_tax = sub_total * (tax_rate/100)
+#DISPLAY SALES TAX
+print("\nSales Tax : " + str(sales_tax))
+#ASK FOR TIP
+check_tip = input("\n Would you like to add a tip? (yes/no): ")
+if check_tip == 'yes' :
+    tip_amount = float(input("Thank you!! \nHow much would you like to tip? : % "))
+    tip_total = sub_total * (tip_amount/100)
+    #COMPUTE TOTAL
+    total = sub_total + sales_tax + tip_total
+
+else :
+    #COMPUTE TOTAL
+    total = sub_total + sales_tax
+
+
+
+#DISPLAY TOTAL
+print("Total : $" + total)
+#GET PAYMENT AMMOUNT
+pay_amount = input("What is the payment amount? : $")
+#DISPLAY CHANGE
+while(pay_amount < total) :
+    pay_amount = input("Please enter a payment amount more than or equal to the total: ")
+    break
+change = pay_amount - total
+print("Your change is : " + change)
+
+#DISPLAY THANK YOU MESSAGE
--- Programming Building Blocks/Week 2/03Teach.py
+++ Programming Building Blocks/Week 2/03Teach.py
@@ -1,3 +1,5 @@
+import math
+
 #Team activity for unit 3
 
 """
@@ -14,25 +16,50 @@ Circle—The area is Pi (you can use 3.14) multiplied by the radius squared.
 """
 #Area of a Square
 #Get Side length of square in cm
+square_length = float(input("What is the side lenght of the square? : "))
 #Calculate area in cm^2
-
+square_area = square_length **2
+#Display area of square
+print("The area of the square is : " + str(square_area) + "cm^2 / " + str(square_area * 10000) + "m^2" )
 #Area of a Rectangle
+
 #Get Length in cm
+rect_length = float(input("What is that length of the rectange? : "))
 #Get width in cm
+rect_width = float(input("What is the width of the rectangle? : "))
 #Calculate area
+rect_area = rect_length*rect_width
 #Display area in cm^2
+print("The area of the rectangle is : " + str(rect_area) + "cm^2 / " + str(rect_area * 10000) + "m^2\n")
+
 
 #Area of a circle
 #Get radius in cm
+circle_rad = float(input("What is the radius of the circle? : "))
 #Stretch number 1 : Use python pi function
 #Calculate area
+# - Imported the math module and uses the .pi method
+circle_area = math.pi*(circle_rad**2)
 #Display area in cm^2
-
-
+print("The area of the circle is : " + str(circle_area) + "cm^2  / " + str(circle_area * 10000) + "m^2/n")
 
 #Stretch 2 : 
 #Ask for single value
+init_value = float(input("Enter an inital value to get areas and volumes : "))
 #Calculate area
+#Area of a square
+square_area_stretch = init_value**2
+#Area of circle
+circle_area_stretch = math.pi*(init_value**2)
 #Calculate Volume
+#Cube Volume
+cube_volume = init_value**3
+#Sphere Volume
+sphere_volume = (4/3) * math.pi * (init_value**3)
 #Display areas
+
+print("The area of the square is : " + str(square_area_stretch))
+print("The area of the circle is : " + str(circle_area_stretch))
 #Display volume
+print("The volume of the cube is  : " + str(cube_volume))
+print("The volume of the sphere : " + str(sphere_volume))
